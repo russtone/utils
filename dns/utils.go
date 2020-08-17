84a97b2d3c61046df5d3d72f0ffc2b121905f047
russtone <russtone@yandex.ru>
2020-08-17 09:55:29 +0300

    Add dns.PTR function

--- dns/utils.go
+++ dns/utils.go
@@ -1,6 +1,10 @@
 package dns
 
-import "strings"
+import (
+	"fmt"
+	"net"
+	"strings"
+)
 
 // Subdomains returns list of subdomains of base domain.
 func Subdomains(sub, base string) []string {
@@ -21,4 +25,31 @@ func Subdomains(sub, base string) []string {
 	}
 
 	return domains
+}
+
+const hexDigit = "0123456789abcdef"
+
+// PTR returns domain name for IP.
+func PTR(ip net.IP) string {
+
+	if ip.To4() != nil {
+		parts := strings.Split(ip.String(), ".")
+		return fmt.Sprintf("%s.%s.%s.%s.in-addr.arpa", parts[3], parts[2], parts[1], parts[0])
+	}
+
+	// Must be IPv6
+	buf := make([]byte, 0, len(ip)*4+len("ip6.arpa"))
+
+	// Add it, in reverse, to the buffer
+	for i := len(ip) - 1; i >= 0; i-- {
+		v := ip[i]
+		buf = append(buf, hexDigit[v&0xF],
+			'.',
+			hexDigit[v>>4],
+			'.')
+	}
+
+	buf = append(buf, "ip6.arpa"...)
+
+	return string(buf)
 }
